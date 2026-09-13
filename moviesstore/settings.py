00e686db3b0fe819710ca3d004d85406dfaeadbe
Aniketh Chadive <ani.chadive@gmail.com>
2026-09-13 17:59:25 -0400

    Switch to SQLite for PythonAnywhere free-tier deployment

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!3tx==)*1^j43j224gy123*244=kmtsx1m3^d@!mzj5$-go_5^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'moviesstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moviesstore',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -125,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
