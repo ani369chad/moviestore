ddb62f5dd7606c37a721da7f7aac40fd8ff9d5c9
Aniketh Chadive <ani.chadive@gmail.com>
2026-09-13 18:38:48 -0400

    edits

--- movies/admin.py
+++ movies/admin.py
@@ -6,6 +6,10 @@ class MovieAdmin(admin.ModelAdmin):
     ordering = ['name']
     search_fields = ['name']
 
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['comment', 'movie', 'user', 'reported']
+    list_filter = ['reported']
+
 admin.site.register(Movie, MovieAdmin)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
 
